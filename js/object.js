2b1858021701b42d84df818181a698f6d9d69e9d
Marat1206 <marikzhekov@gmail.com>
2021-06-27 20:43:11 +0300

    init

---
+++ js/object.js
@@ -0,0 +1,37 @@
+// let user = {
+//     name: 'John',
+//     surname: 'Smith',
+// }
+// user.name = 'Pete';
+// delete user.name;
+//
+// console.log(user);
+
+
+// Можно ли изменить объект, объявленный с помощью const?
+//
+// const user = { ------- константу user мы не можем изменять!
+//
+//   name: «John» -------- мы можем изменять только ключи в нутри ее
+//
+// };
+//
+// // это будет работать?
+//
+// user.name = «Pete»; ------ тут значение name: «John» измениться на «Pete»
+
+
+
+// let salaries = {
+//
+//     John: 100,
+//
+//     Ann: 160,
+//
+//     Pete: 130
+//
+// }
+//
+// let sum = salaries.John + salaries.Ann + salaries.Pete;
+//
+// console.log(sum);
